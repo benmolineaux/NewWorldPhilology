cf48bfaaade19355cf752e692fae80208bae43d0
Benjamin Molineaux <benjamin.molineaux@ed.ac.uk>
2024-07-10 23:24:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,12 +19,12 @@ map.on('drag', function() {
 });
 
 // Add markers and bind popups with hyperlinks to documents
-var marker1 = L.marker([-38.8684, -73.2514]).addTo(map);
+var marker1 = L.marker([-38.8684, -73.17]).addTo(map);
 marker1.bindPopup('<a href="documents/LecturasAraucanas.pdf" target="_blank">Isolate: Mapudungun — Valdivia 1910</a>').openPopup();
 
 var marker2 = L.marker([-23.5505, -46.6333]).addTo(map); // Example coordinates for São Paulo
 marker2.bindPopup('<a href="documents/document2.pdf" target="_blank">Document 2</a>').openPopup();
 
-var marker2 = L.marker([-32.8895, -68.8458]).addTo(map); // Example coordinates for Mendoza
+var marker3 = L.marker([-32.8895, -68.8458]).addTo(map); // Example coordinates for Mendoza
 marker2.bindPopup('<a href="documents/Millcayac.pdf" target="_blank">Huarpe:Millcayac – Valdivia 1607</a>').openPopup();
 
