e0c5f096452a67a58d0335deced762d5af4c6175
Benjamin Molineaux <benjamin.molineaux@ed.ac.uk>
2024-07-11 13:00:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Initialize the map centered on South America with a specific zoom level
-var map = L.map('map').setView([-33.4372, -69.5885], 3.5); // Coordinates for Mendoza
+var map = L.map('map').setView([-33.7833, -47.8667], 4); // Coordinates roughly center of South America
 
 // Set up the OpenStreetMap layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -19,29 +19,32 @@ map.on('drag', function() {
 });
 
 // Custom red marker icon
-//var redMarker = L.AwesomeMarkers.icon({
-  //  icon: 'info-sign',
-    //markerColor: 'red'
-//});
+var redMarker = L.AwesomeMarkers.icon({
+    icon: 'info-sign',
+    markerColor: 'red'
+});
 
 // Function to update the info box content
 function updateInfoBox(content) {
     document.getElementById('info-content').innerHTML = content;
 }
 
-// Add a marker for Mendoza
-var markerMendoza = L.marker([-32.8895, -68.8458]).addTo(map); // Coordinates for Mendoza
-markerMendoza.bindPopup('<b>Huarpe:Millcayac – Valdivia 1607</b>');
+// Add a red marker for Mendoza
+var markerMendoza = L.marker([-32.8895, -68.8458], {icon: redMarker}).addTo(map);
+markerMendoza.bindPopup('<b>Mendoza</b>');
 markerMendoza.on('click', function() {
     updateInfoBox('<h2>Mendoza</h2><p>Details about Mendoza.</p>');
 });
 
-// Add markers and bind popups with hyperlinks to documents
-var marker1 = L.marker([-15.7833, -47.8667]).addTo(map);
+// Add other markers and bind popups with hyperlinks to documents
+var marker1 = L.marker([-15.7833, -47.8667], {icon: redMarker}).addTo(map);
 marker1.bindPopup('<a href="document1.pdf" target="_blank">Document 1</a>');
 marker1.on('click', function() {
     updateInfoBox('<h2>Marker 1</h2><p>Details about Marker 1.</p>');
 });
 
-//var marker2 = L.marker([-23.5505, -46.6333]).addTo(map); // Example coordinates for São Paulo
-//marker2.bindPopup('<a href="document2.pdf" target="_blank">Document 2</a>').openPopup();
+var marker2 = L.marker([-23.5505, -46.6333], {icon: redMarker}).addTo(map); // Example coordinates for São Paulo
+marker2.bindPopup('<a href="document2.pdf" target="_blank">Document 2</a>');
+marker2.on('click', function() {
+    updateInfoBox('<h2>Marker 2</h2><p>Details about Marker 2.</p>');
+});
